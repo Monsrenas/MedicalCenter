c15448fbe8dc76770c4fd01fefedc30e55fb6062
Monsrenas <alcadytoo@gmail.com>
2019-10-25 23:30:01 -0500

    Text on server 3

--- resources/views/consultation/PhysicalExamination.blade.php
+++ resources/views/consultation/PhysicalExamination.blade.php
@@ -119,8 +119,8 @@ global  $patient1;
 	         break; 
 	        } 
 	 
-	 $vlr=(count($valor)==1)?$valor:'';
-	 dd('Aqui');
+	 $vlr=(isset($valor[1]))? "":$valor;
+	
 	 if ($nom) {   $ctrlr="<input type='text' name='".$nom."' size='5' value='".$vlr."' onkeypress='return soloNumeros(event, this.value)'>";
 				$resu="<td colspan='".substr($cdn, 1,1)."'>".substr($cdn, 2).$ctrlr.$unit." </td>";
 				return $resu;
