9b8993d70e88ac70b3e1f8c7264d9b35f6244f6b
Monsrenas <alcadytoo@gmail.com>
2019-11-12 13:03:15 -0500

    Cambio de formato de nota medicas a sistema SOAP

--- resources/views/consultation/Load.blade.php
+++ resources/views/consultation/Load.blade.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION)){ session_start(); }
 	        ( isset($_SESSION['dr_user']       ) )
 	                                                 )?$_SESSION['identification'].$_SESSION['dr_user']: "";
 	
-	if (!isset($_SESSION['identification'])) { return ;}
+	if ((!isset($_SESSION['identification']))or(!isset($_SESSION['dr_user']))) { return ;}
  
 ?>
 <script type="text/javascript">PreLoadDataInView('#left_wind', '&modelo=Interrogation&url=consultation.list&_method=get&findit={{$busca}}', 'flexlist');</script>
