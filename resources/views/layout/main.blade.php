56e2f4b3bbac1dcc4877dcac31b741b17b0f25cd
Monsrenas <alcadytoo@gmail.com>
2019-11-21 15:52:23 -0500

    Tutorial

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -18,7 +18,20 @@
     <div class="row" id="work" style="margin-top: 120px;">
       @include('modal')
       <div class="col-2 col-md-2" id="left_wind" style="margin-left: 5px;"></div>
-      <div class="col-10 col-md-10" id="center_wind" style="background: #E2E2E2; min-height: 385px; max-height: 385px; margin-right: -16px; text-align: center;  overflow: auto scroll;"></div>
+      <div class="col-10 col-md-10" id="center_wind" style="background: #E2E2E2; min-height: 385px; max-height: 385px; margin-right: -16px; text-align: center;  overflow: auto scroll;"> 
+        <div class="contenidoCentro" style="text-align: justify; margin: 50px;">
+          <p>Welcome to the <strong>MEDICAL CENTER</strong> clinical information management application</p> <br>
+          <p> All operations to be performed are registered associated with a patient.</p>
+          <p>It is necessary to activate the patient with whom you will perform the desired action. To do this, perform the following actions:</p> <br>
+          <ul>- Click on the <span class="btn btn-default">Patient</span>  option</ul>
+          <ul>- Click on the <span class="btn btn-primary"> List </span> button</ul>
+          <ul>- Enter the search occurrence or leave it blank to see the entire list.</ul>
+          <ul>- Click on the <span class="btn btn-default glyphicon glyphicon-search"> Patient</span> search button</ul>
+          <ul>- Select the patient by clicking on the name.</ul> <br>
+
+          <p>Any activity that shows a <span class="btn btn-primary glyphicon glyphicon-floppy-save"> Save</span>  button must be saved by the user, otherwise, the information will be lost when you close the section.</p>
+        </div>
+      </div>
       <!--<div class="col-2 col-md-2" id="right_wind" style="margin-right: 5px; margin-left: 6px; padding-left: 30px;"></div> -->
     </div>
 	
